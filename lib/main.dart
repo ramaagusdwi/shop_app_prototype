6a4f31fd06e0cf2e738cdef999fa00b7a085e5e6
Rama Agus <rama.agus@majoo.id>
2023-04-26 15:55:21 +0700

    delte google services.json

--- lib/main.dart
+++ lib/main.dart
@@ -20,7 +20,7 @@ import 'package:shamo/providers/wishlist_provider.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
+  // await Firebase.initializeApp();
   runApp(MyApp());
 }
 
